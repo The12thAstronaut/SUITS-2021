52a7d569ddb10e1815c2450211f3d25aa2e413ca
HorseEars <32291939+HorseEars@users.noreply.github.com>
2020-10-23 14:29:43 -0500

    Added a basic task menu script

---
+++ MoonVR/Assets/TaskManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class TaskManager : MonoBehaviour
+{
+
+    public TextMeshPro titleLabel;
+
+    public UnityEvent onChangedTask;
+    private Task currentTask;
+    private List<Task> tasks = new List<Task>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (titleLabel != null)
+        {
+            titleLabel.SetText("Hello world");
+        }
+
+    }
+}
+
+public class Task
+{
+    public string taskName;
+    public string text;
+    public Destination taskDestination;
+}
