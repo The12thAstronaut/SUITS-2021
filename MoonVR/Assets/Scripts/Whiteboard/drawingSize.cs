4e522827bde88db2e76dfae269a5335f3122b167
georgecoltstegg <54828642+georgecoltstegg@users.noreply.github.com>
2020-10-26 18:11:48 -0500

    Script to change drawing size integrated
    The slider now effects the draw size

---
+++ MoonVR/Assets/Scripts/Whiteboard/drawingSize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class drawingSize : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject drawPrefab;
+
+    public void splineSizeChanger(SliderEventData eventData)
+    {
+        drawPrefab.transform.localScale = new Vector3(0.1f * eventData.NewValue, 0.1f * eventData.NewValue, 0.1f * eventData.NewValue);
+    }
+}
