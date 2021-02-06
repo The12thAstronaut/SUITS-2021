90a67acfa2b2ea33657102497c5e635c1e5ff9ae
lauren_brady <laurenbrady01@tamu.edu>
2021-02-05 22:11:15 -0600

    did i do this too??
    i found this is the stash. no clue either

---
+++ MoonVR/Assets/boxHologram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxHologram : MonoBehaviour
+{
+    
+    private BoundingBox bbox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        bbox = cube.AddComponent<BoundingBox>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
