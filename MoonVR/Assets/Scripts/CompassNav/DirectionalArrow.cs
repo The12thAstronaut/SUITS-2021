331bd3febc2d0bb0845183dd0f97ebcbdcff453f
mannycarr214 <43549928+mannycarr214@users.noreply.github.com>
2021-02-06 19:12:02 -0600

    Navigational Component V2.0
    Ive created a script based on some videos on youtube that allows for a game object to point towards the direction that the user specifies under "target". However, more work is required to fix the axis that the object uses to point towards its target (z axis)
    
    Demo: https://youtu.be/FRSNvYD585o

---
+++ MoonVR/Assets/Scripts/CompassNav/DirectionalArrow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [NOTE] : Current script that is being used. V 2.0.1
+// Still need to translate in order to add comments [French to English]
+public class DirectionalArrow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void Update()
+    {
+        Vector3 targetPosition = target.transform.position;
+        targetPosition.y = transform.position.y;
+
+        // Chooses which gameobject the script will point towards (currently stuck with looking with Z axis of pointer)
+        transform.LookAt(targetPosition);
+    }
+}
