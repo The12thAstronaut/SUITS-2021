03c8f422f002850ab2335fae6526445aebea3ecd
lauren_brady <laurenbrady01@tamu.edu>
2021-02-08 21:50:24 -0600

    switch stuff also task manager
    made the switch animation
    made a task manager for my demos

---
+++ MoonVR/Assets/SwitchHologram.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHologram : MonoBehaviour
+{
+    public GameObject hologramSwitch;
+    public GameObject switchUp;
+    public GameObject switchDown;
+    Quaternion downPos;
+    Quaternion upPos;
+    bool switchFlip = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        hologramSwitch.SetActive(true);
+        switchUp.SetActive(false);
+
+        downPos = switchDown.transform.rotation;
+        upPos = switchUp.transform.rotation;
+
+        var switchDownColor = switchDown.GetComponent<Renderer>().material.color.a;
+        switchDownColor = 0.25f;
+
+        var switchUpColor = switchUp.GetComponent<Renderer>().material.color.a;
+        switchUpColor = 0.25f;
+    }
+
+    IEnumerator SwitchAnimator()
+    {
+
+        yield return new WaitForSeconds(1);
+        switchDown.transform.rotation = downPos;
+
+       
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+
+
+        if (Mathf.Abs(Quaternion.Angle(hologramSwitch.transform.rotation, upPos)) > 5) 
+        {
+                       
+            switchDown.transform.rotation = Quaternion.RotateTowards(switchDown.transform.rotation, upPos, 50 * Time.deltaTime);
+
+            if (switchDown.transform.rotation == upPos)
+            {
+                StartCoroutine (SwitchAnimator());
+
+            }
+
+        }
+
+
+        else
+        {
+            switchDown.SetActive(false);
+
+            hologramSwitch.transform.rotation = upPos;
+
+        }
+
+    }
+}
---
+++ MoonVR/Assets/TaskManager.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TaskManager : MonoBehaviour
+{
+    public GameObject[] taskSet;
+    public Interactable[] taskButtons;
+
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var task in taskSet)
+        {
+            task.SetActive(false);
+        }
+
+        for (int i = 0; i < taskButtons.Length; i++)
+        {
+            int closureIndex = i; 
+            taskButtons[closureIndex].OnClick.AddListener(() => TaskOnClick(closureIndex));
+        }
+
+    }
+
+
+    public void TaskOnClick(int buttonIndex)
+    {
+        count++;
+
+        foreach (var task in taskSet)
+        {
+            task.SetActive(false);
+        }
+
+        if (count % 2 == 0)
+        {
+            taskSet[buttonIndex].SetActive(false);
+        }
+
+        else
+        {
+            taskSet[buttonIndex].SetActive(true);
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
