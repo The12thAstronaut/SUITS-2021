43c0b34e76878c54a87f78a2fa0bf37037871f06
Neil McHenry <neil.g.mchenry@nasa.gov>
2021-03-19 13:47:23 -0500

    Created brockWaypoints.cs
    This script references game objects and pritns out the position coordinates

---
+++ MoonVR/Assets/Scripts/brockWaypoints.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brockWaypoints : MonoBehaviour
+{
+
+    public GameObject waypointOne;
+    public GameObject waypointTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(waypointOne.transform.position);
+        print(waypointTwo.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
