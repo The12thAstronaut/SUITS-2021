d75f94428b2173d8707e1c21d708fee58cfa0cac
lauren_brady <laurenbrady01@tamu.edu>
2021-03-27 23:34:46 -0500

    i think i have it working now
    important note!! include the iteration name in the object description function string (can find under publish information in azure)

--- MoonVR/Assets/MRTK.Tutorials.AzureCloudServices/Scripts/Managers/ObjectDetectionManager.cs
+++ MoonVR/Assets/MRTK.Tutorials.AzureCloudServices/Scripts/Managers/ObjectDetectionManager.cs
@@ -194,7 +194,7 @@ namespace MRTK.Tutorials.AzureCloudServices.Scripts.Managers
             {
                 client.DefaultRequestHeaders.Add("Prediction-Key", apiKey);
                 var result = await client.PostAsync(
-                    $"{resourceBaseEndpoint}/customvision/v3.0/prediction/{projectId}/classify/iterations/{publishedName}/image",
+                    $"{resourceBaseEndpoint}/customvision/v3.0/prediction/{projectId}/classify/iterations/Iteration3/image", // put the iteration here
                     new ByteArrayContent(image));
 
                 if (!result.IsSuccessStatusCode)
