381ce67f802c6d17b82273eec64bc85d5b6ce22f
lauren_brady <laurenbrady01@tamu.edu>
2021-04-01 23:04:54 -0500

    Made a camera capture script

---
+++ MoonVR/Assets/LaurenCustomVision.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.WebCam;
+
+
+public class LaurenCustomVision : MonoBehaviour
+{
+
+    void Start()
+    {
+        InvokeRepeating("Analyze", 5f, 5f);
+    }
+
+
+    private PhotoCapture photoCapture = null;
+
+    private const string FILE_NAME = @"cognitive_analysis.jpg";
+
+    // This method request to create a PhotoCapture object.
+    // When its finish created, call the OnPhotoCreated method.
+    private void Analyze()
+    {
+        PhotoCapture.CreateAsync(false, this.OnPhotoCreated);
+    }
+
+    // This method store the PhotoCapture object just created and retrieve the high quality
+    // available for the camera and then request to start capturing the photo with the
+    // given camera parameters.
+    private void OnPhotoCreated(PhotoCapture captureObject)
+    {
+        this.photoCapture = captureObject;
+
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+
+        CameraParameters c = new CameraParameters()
+        {
+            hologramOpacity = 0.0f,
+            cameraResolutionWidth = cameraResolution.width,
+            cameraResolutionHeight = cameraResolution.height,
+            pixelFormat = CapturePixelFormat.BGRA32
+        };
+        captureObject.StartPhotoModeAsync(c, this.OnPhotoModeStarted);
+    }
+
+    // This method is called when we have access to the camera and can take photo with it.
+    // We request to take the photo and store it in the storage.
+    private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
+    {
+        if (result.success)
+        {
+            string filename = string.Format(FILE_NAME);
+            string filePath = Path.Combine(Application.persistentDataPath, filename);
+            this.photoCapture.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, this.OnCapturedPhotoToDisk);
+        }
+        else
+        {
+            Debug.LogError("Unable to start photo mode.");
+        }
+    }
+
+    // This method is called when the photo is finish taked (or not, so check the succes property)
+    // We can read the file from disk and do anything we need with it.
+    // Finally, we request to stop the photo mode to free the resource.
+    private void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
+    {
+        if (result.success)
+        {
+            string filename = string.Format(FILE_NAME);
+            string filePath = Path.Combine(Application.persistentDataPath, filename);
+
+            byte[] image = File.ReadAllBytes(filePath);
+
+            // We have the photo taken.
+            Debug.Log(filePath);
+        }
+        else
+        {
+            Debug.LogError("Failed to save Photo to disk.");
+        }
+        this.photoCapture.StopPhotoModeAsync(this.OnStoppedPhotoMode);
+    }
+
+    // This method is called when the photo mode is stopped and we can dispose the resources allocated.
+    private void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        this.photoCapture.Dispose();
+        this.photoCapture = null;
+    }
+}
