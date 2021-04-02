10e5dcf1aa7da30e5fe7fe3aa4e1a7e7b37a15ce
Isreal <izzygomezthree@gmail.com>
2021-04-02 03:12:33 -0500

    Made New Cursor
    - Made new cursor
    - Some Arduino configurations were made
    
    Co-Authored-By: Laurenbrady01 <55027585+Laurenbrady01@users.noreply.github.com>

--- MoonVR/Assets/Scripts/ArduinoTrial1.cs
+++ MoonVR/Assets/Scripts/ArduinoTrial1.cs
@@ -10,27 +10,55 @@ using Uduino;
 public class ArduinoTrial1 : MonoBehaviour
 {
     
-    public GameObject Panel;
+    public GameObject panelOne;
+    public GameObject panelTwo;
+    public GameObject panelThree;
     
     // Start is called before the first frame update
     void Start()
     {
         UduinoManager.Instance.pinMode(2, PinMode.Input_pullup);
-        Panel.SetActive(false);
+        UduinoManager.Instance.pinMode(4, PinMode.Input_pullup);
+        UduinoManager.Instance.pinMode(6, PinMode.Input_pullup);
+        panelOne.SetActive(false);
+        panelTwo.SetActive(false);
+        panelThree.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-       int buttonValue = UduinoManager.Instance.digitalRead(2);
+       int buttonValuePin2 = UduinoManager.Instance.digitalRead(2);
+       int buttonValuePin4 = UduinoManager.Instance.digitalRead(4);
+       int buttonValuePin6 = UduinoManager.Instance.digitalRead(6);
         
-        if (buttonValue == 0)
+        if (buttonValuePin2 == 1)
         {
-            Panel.SetActive(true);
+            panelOne.SetActive(true);
         }
         else
         {
-            Panel.SetActive(false);
+            panelOne.SetActive(false);
+        } 
+
+
+        if (buttonValuePin4 == 1)
+        {
+            panelTwo.SetActive(true);
+        }
+        else
+        {
+            panelTwo.SetActive(false);
+        } 
+
+
+        if (buttonValuePin6 == 1)
+        {
+            panelThree.SetActive(true);
+        }
+        else
+        {
+            panelThree.SetActive(false);
         } 
     }
 }
