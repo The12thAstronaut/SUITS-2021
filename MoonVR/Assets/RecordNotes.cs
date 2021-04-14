2ce97ef7d50c27c30b20606b3bd636c578bf80f2
jamsteh <jav3863@tamu.edu>
2021-04-14 02:35:42 -0500

    Added lunarpedia and log/picture sample prefabs

---
+++ MoonVR/Assets/RecordNotes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RecordNotes : MonoBehaviour
+{
+    public GameObject LogPanel;
+    public GameObject SamplePanel;
+    public GameObject LunarpediaPanel;
+    public TextMeshPro LogDescriptionText;
+    public TextMeshPro SampleDescriptionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateLogPanel(string textLog)
+    {
+        LogDescriptionText.text = textLog;
+        GameObject logPanel = Instantiate(LogPanel);
+        logPanel.transform.SetParent(LunarpediaPanel.transform);
+    }
+
+    void CreateSamplePanel(string textSample, Material picture)
+    {
+        SampleDescriptionText.text = textSample;
+
+        GameObject samplePanel = Instantiate(LogPanel);
+        LogPanel.transform.SetParent(LunarpediaPanel.transform);
+
+    }
+
+
+}
