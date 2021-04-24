5f2a495e321fcec82d94f993eee94393e9d04f15
lauren_brady <laurenbrady01@tamu.edu>
2021-04-24 13:38:18 -0500

    possibly rendering the tooltip label?

--- MoonVR/Assets/LaurenCustomVision.cs
+++ MoonVR/Assets/LaurenCustomVision.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Windows.WebCam;
-using MRTK.Tutorials.AzureCloudServices.Scripts.Dtos;
+using CustomVision;
 using Newtonsoft.Json;
 using UnityEngine.Networking;
 using Microsoft.MixedReality.Toolkit.UI;
-using TMPro;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
 
 public class LaurenCustomVision : MonoBehaviour
 {
@@ -18,23 +20,31 @@ public class LaurenCustomVision : MonoBehaviour
     [SerializeField]
     private string _predictionKey = "<your prediction key here>";
 
+    [SerializeField]
+    private GameObject _labelObject;
+
+    [SerializeField]
+    private GameObject _labelContainer;
+
     public Interactable startButton;
     public Interactable stopButton;
 
-
-    public TextMeshPro tagText;
-    public TextMeshPro probabilityText;
-    public TextMeshPro[] boundsText;
-
     private PhotoCapture photoCapture = null;
+    private Resolution _cameraResolution;
 
     private const string FILE_NAME = @"cognitive_analysis.jpg";
+
+    private List<GameObject> _createdObjects = new List<GameObject>();
+    private static int _meshPhysicsLayer = 0;
 
 
     void Start()
     {
         startButton.OnClick.AddListener(() => PhotoStart());
         stopButton.OnClick.AddListener(() => PhotoStop());
+
+        _cameraResolution =
+            PhotoCapture.SupportedResolutions.OrderByDescending(res => res.width * res.height).First();
 
     }
 
@@ -65,16 +75,17 @@ public class LaurenCustomVision : MonoBehaviour
     {
         this.photoCapture = captureObject;
 
-        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-
         CameraParameters c = new CameraParameters()
         {
             hologramOpacity = 0.0f,
-            cameraResolutionWidth = cameraResolution.width,
-            cameraResolutionHeight = cameraResolution.height,
+            cameraResolutionWidth = _cameraResolution.width,
+            cameraResolutionHeight = _cameraResolution.height,
             pixelFormat = CapturePixelFormat.BGRA32
         };
+
+
         captureObject.StartPhotoModeAsync(c, this.OnPhotoModeStarted);
+   
     }
 
     // This method is called when we have access to the camera and can take photo with it.
@@ -107,7 +118,7 @@ public class LaurenCustomVision : MonoBehaviour
 
             // We have the photo taken.
             Debug.Log(filePath);
-            StartCoroutine(RecognizeObjectsInternal(image));
+            StartCoroutine(RecognizeObjectsInternal(image, _cameraResolution, CopyCameraTransForm()));
         }
         else
         {
@@ -123,8 +134,17 @@ public class LaurenCustomVision : MonoBehaviour
         this.photoCapture = null;
     }
 
+    private Transform CopyCameraTransForm()
+    {
+        var g = new GameObject();
+        g.transform.position = Camera.main.transform.position;
+        g.transform.rotation = Camera.main.transform.rotation;
+        g.transform.localScale = Camera.main.transform.localScale;
+        return g.transform;
+    }
 
-    private IEnumerator RecognizeObjectsInternal(IEnumerable<byte> image)
+
+    private IEnumerator RecognizeObjectsInternal(IEnumerable<byte> image, Resolution cameraResolution, Transform cameraTransform)
     {
         var request = UnityWebRequest.Post(_liveDataUrl, string.Empty);
         request.SetRequestHeader("Prediction-Key", _predictionKey);
@@ -132,93 +152,119 @@ public class LaurenCustomVision : MonoBehaviour
         request.uploadHandler = new UploadHandlerRaw(image.ToArray());
         yield return request.SendWebRequest();
         var text = request.downloadHandler.text;
-        var result = JsonConvert.DeserializeObject<ImageQuicktestResult>(text);
-
+        var result = JsonConvert.DeserializeObject<CustomVisionResult>(text);
 
         if (result != null)
         {
-            var name = new List<string>();
-            var prob = new List<double>();
-            var boundL = new List<double>();
-            var boundT = new List<double>();
-            var boundW = new List<double>();
-            var boundH = new List<double>();
 
-            var maxProb = 0d;
-            var maxTag = "";
-            var bounds = new List<double>();
+            result.Predictions.RemoveAll(p => p.Probability < 0.9);
+            Debug.Log("#Predictions = " + result.Predictions.Count());
 
-
-            foreach (var p in result.Predictions)
-            {
-                name.Add(p.TagName);
-                prob.Add(p.Probability);
-                boundL.Add(p.BoundingBox["left"]);
-                boundT.Add(p.BoundingBox["top"]);
-                boundW.Add(p.BoundingBox["width"]);
-                boundH.Add(p.BoundingBox["height"]);
-
-
-            }
-
-            for (int i = 0; i < prob.Count; i++)
-            {
-
-                if (prob[i] < 0.8d)
-                {
-                    prob.Remove(prob[i]);
-                    name.Remove(name[i]);
-                    boundL.Remove(boundL[i]);
-                    boundT.Remove(boundT[i]);
-                    boundW.Remove(boundW[i]);
-                    boundH.Remove(boundH[i]);
-                }
-
-                else
-                {
-                    if (prob[i] > maxProb)
-                    {
-                        bounds.Clear();
-                        maxProb = prob[i];
-                        maxTag = name[i];
-                        bounds.Add(boundL[i]);
-                        bounds.Add(boundT[i]);
-                        bounds.Add(boundW[i]);
-                        bounds.Add(boundH[i]);
-
-                    }
-                }
-
-            }
-
-            if (maxTag != "")
-            {
-                tagText.SetText(maxTag);
-                probabilityText.SetText(maxProb.ToString());
-
-                for (int i = 0; i < bounds.Count; i++)
-                {
-                    boundsText[i].SetText(bounds[i].ToString());
-                }
-            }
-
-            else
-            {
-                tagText.SetText("none");
-                probabilityText.SetText("<80%");
-
-                for (int i = 0; i < bounds.Count; i++)
-                {
-                    boundsText[i].SetText("none");
-                }
-            }
-
+            LabelObjects(result.Predictions, cameraResolution, cameraTransform);
 
         }
         else
         {
             Debug.Log("Predictions is null");
         }
+
+        
     }
+
+    public virtual void LabelObjects(IList<Prediction> predictions,
+        Resolution cameraResolution, Transform cameraTransform)
+    {
+        ClearLabels();
+        var heightFactor = cameraResolution.height / cameraResolution.width;
+        var topCorner = cameraTransform.position + cameraTransform.forward -
+                        cameraTransform.right / 2f +
+                        cameraTransform.up * heightFactor / 2f;
+        foreach (var prediction in predictions)
+        {
+            var _labelText = prediction.TagName;
+            var center = prediction.GetCenter();
+            var recognizedPos = topCorner + cameraTransform.right * center.x -
+                                cameraTransform.up * center.y * heightFactor;
+
+#if UNITY_EDITOR
+            _createdObjects.Add(CreateLabel(_labelText, recognizedPos));
+#endif
+            var labelPos = DoRaycastOnSpatialMap(cameraTransform, recognizedPos);
+            
+            if (labelPos != null)
+            {
+                _createdObjects.Add(CreateLabel(_labelText, labelPos.Value));
+            }
+        }
+
+        Destroy(cameraTransform.gameObject);
+    }
+
+    private Vector3? DoRaycastOnSpatialMap(Transform cameraTransform, Vector3 recognitionCenterPos)
+    {
+        RaycastHit hitInfo;
+
+        var spatialMappingConfig =
+            CoreServices.SpatialAwarenessSystem.ConfigurationProfile as
+                MixedRealitySpatialAwarenessSystemProfile;
+
+        if (spatialMappingConfig != null && Physics.Raycast(cameraTransform.position, (recognitionCenterPos - cameraTransform.position),
+                out hitInfo, 10, GetSpatialMeshMask()))
+        {
+            return hitInfo.point;
+        }
+        return null;
+    }
+
+    private static int GetSpatialMeshMask()
+    {
+        if (_meshPhysicsLayer == 0)
+        {
+            var spatialMappingConfig =
+              CoreServices.SpatialAwarenessSystem.ConfigurationProfile as
+                MixedRealitySpatialAwarenessSystemProfile;
+            if (spatialMappingConfig != null)
+            {
+                foreach (var config in spatialMappingConfig.ObserverConfigurations)
+                {
+                    var observerProfile = config.ObserverProfile
+                        as MixedRealitySpatialAwarenessMeshObserverProfile;
+                    if (observerProfile != null)
+                    {
+                        _meshPhysicsLayer |= (1 << observerProfile.MeshPhysicsLayer);
+                    }
+                }
+            }
+        }
+
+        return _meshPhysicsLayer;
+    }
+
+    private void ClearLabels()
+    {
+        foreach (var label in _createdObjects)
+        {
+            Destroy(label);
+        }
+        _createdObjects.Clear();
+    }
+
+    private GameObject CreateLabel(string text, Vector3 location)
+    {
+        var labelObject = Instantiate(_labelObject);
+        var toolTip = labelObject.GetComponent<ToolTip>();
+        toolTip.ShowBackground = true;
+        toolTip.ToolTipText = text;
+        toolTip.transform.position = location + Vector3.up * 0.2f;
+        toolTip.transform.parent = _labelContainer.transform;
+        toolTip.AttachPointPosition = location;
+        toolTip.ContentParentTransform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        var connector = toolTip.GetComponent<ToolTipConnector>();
+        connector.PivotDirectionOrient = ConnectorOrientType.OrientToCamera;
+        connector.Target = labelObject;
+        return labelObject;
+    }
+
+
 
 }
--- MoonVR/Assets/MRTK.Tutorials.AzureCloudServices/Scripts/Dtos/ImageQuicktestResult.cs
+++ MoonVR/Assets/MRTK.Tutorials.AzureCloudServices/Scripts/Dtos/ImageQuicktestResult.cs
@@ -1,23 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using Newtonsoft.Json;
 
-namespace MRTK.Tutorials.AzureCloudServices.Scripts.Dtos
+namespace CustomVision
 {
-    public class ImageQuicktestResult
+    public class CustomVisionResult
     {
         [JsonProperty("id")]
-        public string Id { get; set; }
+        public Guid Id { get; set; }
 
         [JsonProperty("project")]
-        public string Project { get; set; }
+        public Guid Project { get; set; }
 
         [JsonProperty("iteration")]
-        public string Iteration { get; set; }
+        public Guid Iteration { get; set; }
 
         [JsonProperty("created")]
         public DateTimeOffset Created { get; set; }
 
         [JsonProperty("predictions")]
-        public Prediction[] Predictions { get; set; }
+        public List<Prediction> Predictions { get; set; }
+    }
+
+    public partial class Prediction
+    {
+        [JsonProperty("probability")]
+        public double Probability { get; set; }
+
+        [JsonProperty("tagId")]
+        public Guid TagId { get; set; }
+
+        [JsonProperty("tagName")]
+        public string TagName { get; set; }
+
+        [JsonProperty("boundingBox")]
+        public BoundingBox BoundingBox { get; set; }
+    }
+
+    public partial class BoundingBox
+    {
+        [JsonProperty("left")]
+        public double Left { get; set; }
+
+        [JsonProperty("top")]
+        public double Top { get; set; }
+
+        [JsonProperty("width")]
+        public double Width { get; set; }
+
+        [JsonProperty("height")]
+        public double Height { get; set; }
+    }
+
+
+    public static class PredictionExtensions
+    {
+        public static Vector2 GetCenter(this Prediction p)
+        {
+            return new Vector2((float)(p.BoundingBox.Left + (0.5 * p.BoundingBox.Width)),
+                (float)(p.BoundingBox.Top + (0.5 * p.BoundingBox.Height)));
+        }
     }
 }
