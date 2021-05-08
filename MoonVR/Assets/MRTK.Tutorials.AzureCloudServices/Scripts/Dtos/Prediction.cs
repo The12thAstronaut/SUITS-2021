1baecd4be6a529fec70b88d9204e292c60b209bb
lauren_brady <laurenbrady01@tamu.edu>
2021-05-08 16:35:09 -0500

    idk what this is

---
+++ MoonVR/Assets/MRTK.Tutorials.AzureCloudServices/Scripts/Dtos/Prediction.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MRTK.Tutorials.AzureCloudServices.Scripts.Dtos
+{
+    public class Prediction
+    {
+        [JsonProperty("probability")]
+        public double Probability { get; set; }
+
+        [JsonProperty("tagId")]
+        public string TagId { get; set; }
+
+        [JsonProperty("tagName")]
+        public string TagName { get; set; }
+
+        [JsonProperty("boundingBox")]
+        public Dictionary<string, double> BoundingBox { get; set; }
+    }
+}
