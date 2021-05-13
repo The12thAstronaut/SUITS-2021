1b90f71c99f7ec0948981d2bfd7614b647c2bfd7
lauren_brady <laurenbrady01@tamu.edu>
2021-05-13 16:15:23 -0500

    voice logs

--- MoonVR/Assets/MRTK.Tutorials.AzureSpeechServices/Scripts/LunarcomController.cs
+++ MoonVR/Assets/MRTK.Tutorials.AzureSpeechServices/Scripts/LunarcomController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public enum RecognitionMode { Speech_Recognizer, Intent_Recognizer, Tralation_Recognizer, Disabled, Offline };
 public enum SimuilateOfflineMode { Enabled, Disabled };
