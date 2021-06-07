94df885734318808d47f61c1621a267b35ef6798
Neil McHenry <neilmchenry@gmail.com>
2021-06-07 13:45:42 -0500

    Created initial version of ClippyScout, which can tell you the current time

---
+++ MoonVR/Assets/Scripts/ClippyScout/ChatBubble.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatBubble : MonoBehaviour
+{
+
+    private SpriteRenderer backgroundSpriteRenderer;
+    private SpriteRenderer iconSpriteRenderer;
+    private TextMeshPro textMeshPro;
+
+    public string timeString;
+
+
+    // When this class begins, find references to chat bubble variables
+    private void Awake()
+    {
+        backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
+        iconSpriteRenderer = transform.Find("Icon").GetComponent<SpriteRenderer>();
+        textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
+    }
+
+    private void Setup(string text)
+    {
+        textMeshPro.SetText("The current time is: " + text);
+        textMeshPro.ForceMeshUpdate();           //Force text to update instantly
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+
+        Vector2 padding = new Vector2(4f, 2f);   //Create padding around text
+        backgroundSpriteRenderer.size = textSize + padding;   //Resize background bubble to match text
+    }
+
+    private void Update()
+    {
+        timeString = string.Format("{0:hh:mm:ss tt}", DateTime.Now);
+        Setup(timeString);
+    }
+}
